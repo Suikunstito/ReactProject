c0e83e4f03f8c3f771139b713c516cbf3e99a9e6
Suikunstito <fabianhsk6@gmail.com>
2024-04-27 11:10:10 -0400

    LoginScreen

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const isLoggedIn = false;
+
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
+      {isLoggedIn ? (
+        <MainTabNavigator />
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+    /* <View style={styles.container}>
       <Text>Open up App.js to start working on your app! cambio</Text>
       <StatusBar style="auto" />
-    </View>
+    </View> ;*/
   );
 }
 
