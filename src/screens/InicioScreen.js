f0980e062fe321232563dd318d21cca031971a17
KevinZer0 <shodow.13@gmail.com>
2024-05-21 21:25:51 -0400

    :v:
    Se agrega passwordrecovery :v

--- src/screens/InicioScreen.js
+++ src/screens/InicioScreen.js
@@ -2,21 +2,35 @@ import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import LoginComponent from '../components/LoginComponent';
 import RegisterComponent from '../components/RegisterComponent';
+import ForgotPasswordComponent from '../components/ForgotPasswordComponent';
 
 const InicioScreen = () => {
-  const [cambiarComponent, setCambiarComponent] = useState(false);
+  const [cambiarComponent, setCambiarComponent] = useState('login'); // Usa 'login' como valor inicial
+  
+  const switchComponent = () => {
+    switch (cambiarComponent) {
+      case 'login':
+        return <LoginComponent />;
+      case 'register':
+        return <RegisterComponent />;
+      case 'forgotPassword':
+        return <ForgotPasswordComponent />;
+      default:
+        return <LoginComponent />;
+    }
+  };
 
   return (
     <View>
       {/* Logo de la app */}
-      {cambiarComponent ? (
-        <RegisterComponent />
-      ) : (
-        <LoginComponent />
-      )}
+      {switchComponent()}
       <Button
-        title={cambiarComponent ? "¿Tienes perfil? Ingresar" : "¿No tienes perfil? Regístrate"}
-        onPress={() => setCambiarComponent(!cambiarComponent)}
+        title={cambiarComponent === 'login' ? "¿No tienes perfil? Regístrate" : "¿Tienes perfil? Ingresar"}
+        onPress={() => setCambiarComponent(cambiarComponent === 'login' ? 'register' : 'login')}
+      />
+      <Button
+        title="¿Has olvidado la contraseña?"
+        onPress={() => setCambiarComponent('forgotPassword')}
       />
     </View>
   );
