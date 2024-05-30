00ad97cea83efa65a570b39e0c0c61298786ba7b
Suikunstito <fabianhsk6@gmail.com>
2024-05-29 23:09:12 -0400

    29-05

--- src/services/apirest.js
+++ src/services/apirest.js
@@ -2,7 +2,7 @@ import Config from 'react-native-config';
 
 const IP = Config.IP_SERVER || '';
 console.log('ip conexion:', IP);
-const API_URL = `http://${IP}:3000/saludo`;  // URL de tu API
+const API_URL = `https://e645-2803-c180-2601-7756-ddf6-4b57-65ab-3e79.ngrok-free.app/saludo`;  // URL de tu API
 
 const obtenerSaludo = async () => {
     try {
