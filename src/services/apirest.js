9be5a9088cb28b41693d8c90f492afb08292044a
Suikunstito <fabianhsk6@gmail.com>
2024-05-30 01:04:25 -0400

    29-05

--- src/services/apirest.js
+++ src/services/apirest.js
@@ -1,19 +1,21 @@
 import Config from 'react-native-config';
 
-const IP = Config.IP_SERVER || '';
-console.log('ip conexion:', IP);
-const API_URL = `https://e645-2803-c180-2601-7756-ddf6-4b57-65ab-3e79.ngrok-free.app/saludo`;  // URL de tu API
+const API_URL = Config.API_URL || '';  // URL de tu API
 
-const obtenerSaludo = async () => {
-    try {
-        const response = await fetch(`${API_URL}`);
-        const data = await response.json();
-        return data.mensaje;
-    } catch (error) {
-        console.error('Error al obtener el saludo:', error);
-        console.error('ip conexion:', API_URL);
-        return null;
-    }
+const APIREST = {
+    obtenerSaludo: async () => {
+        try {
+            const response = await fetch(API_URL); // Usar la variable API_URL
+            const data = await response.json();
+            return data.mensaje;
+        } catch (error) {
+            console.error('Error al obtener el saludo:', error);
+            console.error('URL de la API:', API_URL);
+            return null;
+        }
+    },
+    
+    
 };
 
-export { obtenerSaludo };
+export default APIREST;
